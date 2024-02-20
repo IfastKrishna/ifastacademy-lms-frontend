da2996c29746d3d43e504fce89a527c1b35662e0
Krishna Prasad <goooooboooo@gmail.com>
2024-02-20 17:15:14 +0530

    Dashbord component created

---
+++ src/pages/Dashbord/Dashbord.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Dashbord() {
+  return (
+    <div>
+      <h1 className="text-3xl font-medium text-gray-600">
+        Please login to see your course and profile
+      </h1>
+    </div>
+  );
+}
+
+export default Dashbord;
